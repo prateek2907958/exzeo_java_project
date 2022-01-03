c3d43f58391fde1f438b7d8c0ba6299b9230d503
Prateek <prateekmittal396@gmail.com>
2022-01-03 20:37:49 +0530

    initial commit

---
+++ src/main/java/com/example/exzeo/ExzeoApplication.java
@@ -0,0 +1,13 @@
+package com.example.exzeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExzeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExzeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exzeo/ExzeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exzeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExzeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
